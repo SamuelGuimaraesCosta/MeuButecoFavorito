e2c16605c8ec0ece0394998cc38bea29d8727573
SamuelGuimaraesCosta <75049214+SamuelGuimaraesCosta@users.noreply.github.com>
2020-12-28 12:27:03 -0300

    mbf_0.0.7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'start',
     loadChildren: () => import('./start/start.module').then( m => m.StartPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
